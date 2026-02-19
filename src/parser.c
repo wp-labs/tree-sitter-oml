994170cc99eecfafe984a0ccb03380ecbf0b7aed
seeker-zuo <seeker-zuo@outlook.com>
2026-02-20 00:03:44 +0800

    up

--- src/parser.c
+++ src/parser.c
@@ -9570,7 +9570,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_oml(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 1,
+      .minor_version = 2,
       .patch_version = 0,
     },
   };
